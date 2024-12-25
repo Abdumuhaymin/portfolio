86cfa8cbf2b125e05277d40699eae12b75bc154a
Abdumuhaymin <uzbfamily57@gmail.com>
2024-12-25 20:27:25 +0500

    refactor Navbar component text and remove unused imports in Home and Socials pages

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,12 @@
-"use client";
 import Link from "next/link";
 
 const Navbar = () => {
+  const text = "Abdirakhmanov/'s";
   return (
     <nav className="container px-4 mx-auto bg-primary text-white py-4 sticky top-0">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">
-          <Link href="/">Abdurakhmanov's</Link>
+          <Link href="/">{text}</Link>
         </h1>
         <ul className="flex space-x-6">
           <li>
--- app/socials/page.tsx
+++ app/socials/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Socials = () => {
