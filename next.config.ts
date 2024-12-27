651a188625fdc70f1b2ca3c0789ac3b786e9fa3b
Abdumuhaymin <uzbfamily57@gmail.com>
2024-12-27 17:58:42 +0500

    update image configuration to use domains instead of remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.postimg.cc",
-      },
-    ],
+    domains: ["i.postimg.cc"],
   },
 };
 
